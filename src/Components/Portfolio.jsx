4ccbd6db647e1ea14955379342c8064eebd275f6
Anupa Baral <86550755+12anupa-baral@users.noreply.github.com>
2025-03-26 21:04:23 +0545

    projects added

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -3,42 +3,61 @@ import Card from './Card';
 
 const Portfolio = () => {
   const Projects = [
+
     {
       id: 1,
+      Imgurl: '/assets/renderbooking.png',
+      Demourl: 'https://renderbooking.com/',
+      Githuburl: '#'
+    },
+    {
+      id: 2,
       Imgurl: '/assets/Catalog.png',
       Demourl: 'https://12anupa-baral.github.io/YoungInnovation_Frontend/',
       Githuburl: 'https://github.com/12anupa-baral/YoungInnovation_Frontend'
     },
     {
-      id: 2,
+      id: 3,
       Imgurl: '/assets/Highstep.png',
       Demourl: 'https://12anupa-baral.github.io/Highstep_Clone/',
       Githuburl: 'https://github.com/12anupa-baral/Highstep_Clone'
     },
     {
-      id: 3,
+      id: 4,
       Imgurl: '/assets/Reactdashboard.png',
       Demourl: 'https://react-dashboard-orpin.vercel.app/',
       Githuburl: 'https://github.com/12anupa-baral/React_dashboard'
 
     },
     {
-      id: 4,
+      id: 5,
       Imgurl: '/assets/Hackernews.png',
       Demourl: 'https://hacker-news-12anupa-barals-projects.vercel.app/',
       Githuburl: 'https://github.com/12anupa-baral/HackerNews'
     },
     {
-      id: 5,
+      id: 6,
       Imgurl: '/assets/Sessami.png',
       Demourl: 'https://12anupa-baral.github.io/Sessami-website/',
       Githuburl: 'https://github.com/12anupa-baral/Sessami-website'
     },
     {
-      id: 6,
+      id: 7,
       Imgurl: '/assets/Github.png',
       Demourl: 'https://12anupa-baral.github.io/Github_ui/',
       Githuburl: 'https://github.com/12anupa-baral/Github_ui'
+    },
+    {
+      id: 8,
+      Imgurl: '/assets/Pomodaro.png',
+      Demourl: 'https://github.com/12anupa-baral/Pomodoro-timer',
+      Githuburl: 'https://12anupa-baral.github.io/Pomodoro-timer/'
+    },
+    {
+      id: 9,
+      Imgurl: '/assets/clocktimer.png',
+      Demourl: 'https://github.com/12anupa-baral/Clock-timer',
+      Githuburl: 'https://12anupa-baral.github.io/Clock-timer/'
     },
   ];
 
